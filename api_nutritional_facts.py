b8d63ab857aafc5920e14bc4244ff1d47d6ae886
Alan Roger <91630631+AllenRodger22@users.noreply.github.com>
2022-03-29 08:58:10 -0300

    fixed dummies errors

--- api_nutritional_facts.py
+++ api_nutritional_facts.py
@@ -1,12 +1,19 @@
-import streamlit as st
+from flask import Flask
+
 import pandas as pd
-import env
-import functions
 
-st.header('My Food Application')
-col1, col2 = st.columns(2)
-with col1:
-    st.image(functions.get_food_image('Orange fruit'), width=300)
+data = pd.read_csv('data_nutrition.csv')
 
-with col2:
-    st.write('Aplicações de informações nutricionais \n obs: eu sei q ta um lixo')
+app = Flask('_myapp_')
+
+@app.route('/food-info/<foodname>',methods=['GET'])
+def get_food_info(foodname):
+  q = data.query(f"product_name == '{foodname}'")
+  protein = q['proteins_100g'].median()
+  fat = q['fat_100g'].median()
+  carb = q['carbohydrates_100g'].median()
+  response = {'nutritional facts:':{'protein_per_100g:':round(protein,2),'fat_per_100g:':round(fat,2),'carbohydrate_per_100g:':round(carb,2)}}
+  
+  return response
+
+app.run(host='0.0.0.0')
